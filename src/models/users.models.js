fa8c106d884247987fdf86a2238e5bb8f3609be8
Irfan Kazi <irfangithub123@gmail.com>
2025-09-11 23:44:49 +0530

    Add user model, API response, and async handler utilities; update dependencies

---
+++ src/models/users.models.js
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken"
+
+
+const userSchema =  new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:[true,"Password is required"]
+    },
+    role:{
+        type:String,
+        enum:["user","admin"],
+        default:"user"
+    },
+    refreshToken:{
+        type:String
+    }
+},{timestamps:true})
+
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next()
+    this.password = await bcrypt.hash(this.password,10)
+    next();
+})
+
+userSchema.methods.ispasswordCorrect = async function(password) {
+    return await bcrypt.compare(password,this.password)
+} 
+
+userSchema.methods.generateRefreshToken = function() {
+    return jwt.sign(
+        {
+        username,email,password
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {expiresIn:process.env.REFRESH_EXPIRY}
+        
+    )
+}
+
+userSchema.methods.generateAccessToken = function() {
+    return jwt.sign(
+        {
+        username,email,password
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {expiresIn:process.env.ACCESS_EXPIRY}
+        
+    )
+}
+export const User = mongoose.model('User',userSchema)
+
